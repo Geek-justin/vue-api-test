83b50e704146035ec1c2e2156de07d2682775c32
zhangji <zhangji@koibone.com>
2021-06-07 18:50:11 +0800

    rebuild

--- pages/api-test/index.js
+++
@@ -1,9 +0,0 @@
-const App = {
-  components: {
-    ApiTest,
-    ApiGroup
-  },
-}
-
-const app = Vue.createApp(App)
-app.use(ElementPlus).mount('#wrapper')
--- popup/index.js
+++
@@ -1,4 +0,0 @@
-;(function () {
-    let bgPage = chrome.extension.getBackgroundPage()
-    bgPage.bgInstance.openExtPages('api-test')
-})()
